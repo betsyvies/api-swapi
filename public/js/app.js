3f75277905f24bd20b6943320864c8d2196757a3
betsyvies <ing.betsyvies@gmail.com>
2018-02-15 05:12:35 -0500

    añadiendo imagenes con la api flickr

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 function begin() {
   const $buttonImages = $('#button-images');
-  const $containerImages = $('#container-images');
+  const $containerImages = $('#container-img');
   
   $buttonImages.on('click', function(e) {
     e.preventDefault();
@@ -32,6 +32,26 @@ function begin() {
     
     character.forEach(function(element) {
       const name = element.name;
+
+      flickrApi = 'https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?'
+      flickrOptions = {
+        tags: name,
+        format: 'json'
+      }
+
+      const renderPhotos = (data) => {
+        console.log(data);
+        const photo = data.items
+
+        photo.forEach(function(element) {
+          // console.log(photo.media);
+          $containerImages.append(`<div class="col-xs-3 col-md-3 col-lg-3">
+          <img src="${element.media.m}" alt="img1" class="img-responsive">
+        </div>`)
+        });
+      }
+
+      $.getJSON(flickrApi, flickrOptions, renderPhotos)
     });
   }
   
